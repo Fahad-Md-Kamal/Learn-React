603d04e1ffbf0158b17ec723beda2e7038e3c9db
fahad-md-kamal <faahad.hossain@gmail.com>
2022-04-22 06:57:18 +0600

    Making Components dynamic for hold variable data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,17 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+    const expenseDate = new Date(2022, 4, 22);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 5.13;
+
+    
   return (
     <div className="expense-item">
-      <div>April 22nd, 2022</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Started Learing React</h2>
-        <div className="expense-item__price">$500.88</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );

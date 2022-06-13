51c4f0dd56de2b24fe86b05b18b4aa2c17741b99
fahad-md-kamal <faahad.hossain@gmail.com>
2022-06-13 10:04:18 +0600

    filtering and rendering based on filter configured

--- src/App.js
+++ src/App.js
@@ -1,36 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
 
 import "./App.css";
 import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
-function App() {
-  const expenses = [
-    {
-      date: new Date(2022, 4, 22),
-      title: "Car Insurance",
-      amount: 5.13,
-    },
-    {
-      date: new Date(2022, 2, 5),
-      title: "Buy Car",
-      amount: 1500000,
-    },
-    {
-      date: new Date(2021, 12, 6),
-      title: "Our Precious",
-      amount: 3000000,
-    },
-    {
-      date: new Date(2022, 1, 2),
-      title: "Last Conversation",
-      amount: 15,
-    },
-  ];
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    date: new Date(2022, 4, 22),
+    title: "Car Insurance",
+    amount: 5.13,
+  },
+  {
+    id: 'e2',
+    date: new Date(2022, 2, 5),
+    title: "Buy Car",
+    amount: 1500000,
+  },
+  {
+    id: 'e3',
+    date: new Date(2021, 12, 6),
+    title: "Our Precious",
+    amount: 3000000,
+  },
+  {
+    id: 'e4',
+    date: new Date(2022, 1, 2),
+    title: "Last Conversation",
+    amount: 15,
+  },
+];
 
-  const addExpenseHandler  = expense => {
-    console.log('In App.js');
-    console.log(expense);
+function App() {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler  = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
 
 

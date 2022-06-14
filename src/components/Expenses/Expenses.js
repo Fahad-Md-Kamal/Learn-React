6ea8c19c6ebd3eeaf1a29ffb3a3a2b899c67e5b0
fahad-md-kamal <faahad.hossain@gmail.com>
2022-06-14 09:28:06 +0600

    Complete of Expenses chart

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
-import Card from "../UI/Card";
 import ExpensesFilter from "./ExpenseFilter";
-import "./Expenses.css";
+import Card from "../UI/Card";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
+
+import "./Expenses.css";
 
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState('2022');
@@ -22,6 +24,7 @@ function Expenses(props) {
         selected={filteredYear} 
         onChangeFilter={filterChangeHandler} 
         />
+      <ExpensesChart expenses={filteredExpenses}/>
       <ExpensesList items={filteredExpenses} />
       
     </Card>

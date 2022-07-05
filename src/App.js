db2a4ad03efdd0ffbbea819453481e073247994c
fahad-md-kamal <faahad.hossain@gmail.com>
2022-07-05 09:27:04 +0600

    React Portal

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import { useState } from 'react';
-import './App.css';
-import AddUser from './components/Users/AddUser';
-import UsersList from './components/Users/UsersList';
+import React, { useState } from "react";
+import "./App.css";
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
 
 function App() {
   const [usersList, setUsersList] = useState([]);
 
   const addUserHandler = (uName, uAge) => {
     setUsersList((prevUsersList) => {
-      return [...prevUsersList, {name: uName, age:uAge, id: Math.random().toString()}];
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
     });
   };
 
-
   return (
-    <div>
-      <AddUser onAddUser={addUserHandler}/>
-      <UsersList users={usersList}/>
-    </div>
+    <>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </>
   );
 }
 

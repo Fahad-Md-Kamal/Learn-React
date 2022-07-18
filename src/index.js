0f588f9b6fe2763bb6d384708891fbe01aba2c88
fahad-md-kamal <faahad.hossain@gmail.com>
2022-07-18 09:19:05 +0600

    Using useContext Hook for reducing component's load

--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { AuthContextProvier } from './store/auth-context';
 
 ReactDOM.render(
   <React.StrictMode>
+    <AuthContextProvier>
     <App />
+    </AuthContextProvier>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
+// to log results (for example: reportWebVitals(co nsole.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
